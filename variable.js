0ca45c39846b4f870bcd2bb5188ed57f39220feb
nagendra_singh <nagendrasinghrawna17@gmail.com>
2024-03-12 03:28:27 -0400

    let,const and var

---
+++ variable.js
@@ -0,0 +1,20 @@
+let accountId = "78776";
+let accountHolderName = "Nagendra Singh";
+let accountPassword = "Gonu7877@#";
+let accountGmail = "Shekhawatgonu@gmail.com";
+let accountUniqueIdentification = "Gonu17";
+
+const data = [{
+    accountId: accountId,
+    accountHolderName: accountHolderName,
+    accountPassword: accountPassword,
+    accountGmail: accountGmail,
+    accountUniqueIdentification: accountUniqueIdentification
+}];
+console.table(data);
+/* 
+let => we change the values of inside that variable ;
+var => we change the values of inside that variable but we dont use beacuse it creates block scope problem or functional scope problem ;
+const => we cant change values inside that variables;
+
+*/
