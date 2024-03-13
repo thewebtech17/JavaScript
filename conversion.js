061f193d66b6e1836697023554f6e95ffb70b6ea
nagendra_singh <nagendrasinghrawna17@gmail.com>
2024-03-13 02:35:47 -0400

    conversion practice

---
+++ conversion.js
@@ -0,0 +1,54 @@
+// => Number to string
+// let aNum = 33;
+// let aString = String(aNum);
+// console.log(aString);
+// console.log(typeof aString);
+
+// => Number to boolean
+// let aNum = 33;
+// let aBool = Boolean(aNum);
+// console.log(aBool);
+// console.log(typeof aBool);
+
+// => string to boolean
+// let aStr = "Gonu";
+// let aString = Boolean(aStr);
+// console.log(aString);
+// console.log(typeof aString);
+
+// => Empty String to boolean  => {empty string always gives false }
+// let aStr = "";
+// let aString = Boolean(aStr);
+// console.log(aString);
+// console.log(typeof aString);
+
+// => boolean to number  {true always gives->1, false always gives->0}
+// let aNum = true;
+// let aString = Number(aNum);
+// console.log(aString);
+// console.log(typeof aString);
+
+// => Null to number {Null to number always gives 0 but the typeof => number}
+// let aNull = null;
+// let bNl = Number(aNull);
+// console.log(bNl);
+// console.log(typeof bNl);
+
+// => Null To String
+// let aNull = null;
+// let bNl = String(aNull);
+// console.log(bNl);
+// console.log(typeof bNl);
+
+// => undefined to number  {it's convert undefined into number but as a NaN}
+// let aBn ;
+// let bBn = Number(aBn);
+// console.log(bBn);
+// console.log(typeof bBn);
+
+//  undefined to boolean 
+// let aBn ;
+// let bBn = Boolean(aBn);
+// console.log(bBn);
+// console.log(typeof bBn);
+
