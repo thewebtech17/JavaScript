ef51bac6d446d2a46b1332bdf4c861c897e880d9
nagendra_singh <nagendrasinghrawna17@gmail.com>
2024-03-14 01:54:47 -0400

    why string to number is so confusion

---
+++ conversion2.js
@@ -0,0 +1,17 @@
+//  String to number
+// console.log(1 + 2 + "2");
+// console.log("1"+2+"2"+3+"4");
+/* => string to number logic firstly a number and second a number or thiird as a string then first two number add and last string as it same
+=> first a string and second a number then as it same */
+
+// postfix and prefix 
+let a = 7;
+let b =8;
+console.log(`a :- ${++a}: b :- ${b--}`) ; // a=8,b=8
+console.log(`a :- ${++a}: b :- ${b--}`) ; // a=9,b=7
+console.log(`a :- ${++a}: b :- ${b--}`) ; // a=10,b=6
+console.log(`a :- ${++a}: b :- ${b--}`) ; // a=11,b=5
+console.log(`a :- ${++a}: b :- ${b--}`) ; // a=12,b=4
+console.log(`a :- ${++a}: b :- ${b--}`) ; //a=13, b=3
+console.log(`a :- ${++a}: b :- ${b--}`) ; // a=14,b=2
+console.log(`a :- ${++a}: b :- ${b--}`) ; // a=15,b=1
