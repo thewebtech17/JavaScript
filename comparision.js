caa07a297a6750ba577b358b1d3b731cf1d9ddeb
nagendra_singh <nagendrasinghrawna17@gmail.com>
2024-03-14 02:11:57 -0400

    comparison null & undefined

---
+++ comparision.js
@@ -0,0 +1,16 @@
+// //  null comparision
+// console.log(null > 0); //false
+// console.log(null < 0); //false
+// console.log(null >= 0); // True
+// console.log(null <= 0); // True
+// console.log(null == 0); //false
+// console.log(null === 0); //false
+
+// undefined comparision
+//  null comparision
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+console.log(undefined >= 0); // false
+console.log(undefined <= 0); // false
+console.log(undefined == 0); //false
+console.log(undefined === 0); //false
