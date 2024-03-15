fe555345b2cb4e7c0cbeafbc70abe8a460ee49ff
nagendra_singh <nagendrasinghrawna17@gmail.com>
2024-03-15 07:22:37 -0400

    string method

---
+++ strings.js
@@ -0,0 +1,26 @@
+let strName = "nagendra";
+let secstr = "singh"
+// let secName = strName;
+// secName.replace = "vignesgh";
+// console.log(secName)
+// console.log(strName)
+
+// => METHODS :- 
+
+//.1-> console.log(strName.charAt(2));  // => given character value at which position this is appear 
+
+//.2-> console.log(strName.charCodeAt(0))   // => give the charcter ASCII CODE
+
+//.3-> console.log(strName.codePointAt(0));  // =>
+
+//.4-> let thirdstr = strName.concat(" ",secstr)
+// console.log(thirdstr)                              //=> merge two strings
+
+//.5-> console.log(strName.endsWith("dma"));  //=> check that defined string end with or not
+
+//.6-> console.log(strName.includes("n"));          //=> check that defined charcter is present or not
+
+//.7->
+console.log(strName.indexOf("a"));
+
+
