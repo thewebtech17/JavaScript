81aa246f44cad2cb593fe805952a3f21c90de88f
nagendra_singh <nagendrasinghrawna17@gmail.com>
2024-03-16 03:32:04 -0400

    math and maths method

---
+++ maths.js
@@ -0,0 +1,14 @@
+const Max = 30;
+const Min = 10;
+we find max and min between in this number (max & min)
+console.log(Math.floor(Math.random() * (Max-Min+1)) + Min)         -->  this is formula
+
+console.log(Math.abs(-4)); --> this method is used for change the negative value into the postivie number but not positive into negative
+
+console.log(Math.round(4.47)); --> this method is used for change the float values into next number
+Math.ceil() --> it is part of math.round() when we used ceil then he return only upper value
+ Math.floor() --> it is part of math.round() when we used floor then he return only lower value
+
+console.log(Math.max(3,5,6,7,8,4,1)); --> it always return max value inside the values 
+
+console.log(Math.min(3,2,3,34,5,6,3))    --> it always return min value inside the values 
