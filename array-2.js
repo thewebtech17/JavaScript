652c7b7d704cbd925e6fe0b7e4077ee02abe6a84
nagendra_singh <nagendrasinghrawna17@gmail.com>
2024-03-21 04:03:34 -0400

    objects-1

--- array-2.js
+++ array-2.js
@@ -1,79 +1,79 @@
-//  merging array problem
+// //  merging array problem
 
-//  first appoarch [push method] :- 
+// //  first appoarch [push method] :- 
 
- let myArr = ["Diljit Dosanjh","Parmish Verma","Laadi Chahal","Honey Singh"];
- let newArr = ["Guru Randhawa","Karan Aujla","Jasmine Sandlas","Akay"];
- myArr.push(newArr);
- console.log(myArr);
-//  conclusion ;- It creates array inside array problem. => 
- [
-  'Diljit Dosanjh',
-  'Parmish Verma',
-  'Laadi Chahal',
-  'Honey Singh',
-  [ 'Guru Randhawa', 'Karan Aujla', 'Jasmine Sandlas', 'Akay' ]
-]
+//  let myArr = ["Diljit Dosanjh","Parmish Verma","Laadi Chahal","Honey Singh"];
+//  let newArr = ["Guru Randhawa","Karan Aujla","Jasmine Sandlas","Akay"];
+//  myArr.push(newArr);
+//  console.log(myArr);
+// //  conclusion ;- It creates array inside array problem. => 
+//  [
+//   'Diljit Dosanjh',
+//   'Parmish Verma',
+//   'Laadi Chahal',
+//   'Honey Singh',
+//   [ 'Guru Randhawa', 'Karan Aujla', 'Jasmine Sandlas', 'Akay' ]
+// ]
 
-//  Second Appoarch [concat] :-
-let myArr = ["Diljit Dosanjh","Parmish Verma","Laadi Chahal","Honey Singh"];
- let newArr = ["Guru Randhawa","Karan Aujla","Jasmine Sandlas","Akay"];
- const original_Arr = myArr.concat(newArr);
- console.log(original_Arr);
-// conclusion ;- it do best it merge the array without array inside array 
-/* 
-[
-  'Diljit Dosanjh',
-  'Parmish Verma', 
-  'Laadi Chahal',  
-  'Honey Singh',
-  'Guru Randhawa',
-  'Karan Aujla',
-  'Jasmine Sandlas',
-  'Akay'
-]
-*/
+// //  Second Appoarch [concat] :-
+// let myArr = ["Diljit Dosanjh","Parmish Verma","Laadi Chahal","Honey Singh"];
+//  let newArr = ["Guru Randhawa","Karan Aujla","Jasmine Sandlas","Akay"];
+//  const original_Arr = myArr.concat(newArr);
+//  console.log(original_Arr);
+// // conclusion ;- it do best it merge the array without array inside array 
+// /* 
+// [
+//   'Diljit Dosanjh',
+//   'Parmish Verma', 
+//   'Laadi Chahal',  
+//   'Honey Singh',
+//   'Guru Randhawa',
+//   'Karan Aujla',
+//   'Jasmine Sandlas',
+//   'Akay'
+// ]
+// */
 
-//  when we have array like this :
-let fcarr = ['Diljit Dosanjh','Parmish Verma','Laadi Chahal','Honey Singh',[ 'Guru Randhawa', 'Karan Aujla', 'Jasmine Sandlas', 'Akay' ]]
+// //  when we have array like this :
+// let fcarr = ['Diljit Dosanjh','Parmish Verma','Laadi Chahal','Honey Singh',[ 'Guru Randhawa', 'Karan Aujla', 'Jasmine Sandlas', 'Akay' ]]
 
-// we solve this from --> flat() method
+// // we solve this from --> flat() method
 
-console.log(fcarr.flat(Infinity));
+// console.log(fcarr.flat(Infinity));
 
-//  conculsion : - it remove array inside array;
-/* 
-[
-  'Diljit Dosanjh',
-  'Parmish Verma',
-  'Laadi Chahal',
-  'Honey Singh',
-  'Guru Randhawa',
-  'Karan Aujla',
-  'Jasmine Sandlas',
-  'Akay'
-]
-*/
+// //  conculsion : - it remove array inside array;
+// /* 
+// [
+//   'Diljit Dosanjh',
+//   'Parmish Verma',
+//   'Laadi Chahal',
+//   'Honey Singh',
+//   'Guru Randhawa',
+//   'Karan Aujla',
+//   'Jasmine Sandlas',
+//   'Akay'
+// ]
+// */
 
-//  when we want to check is array or not
+// //  when we want to check is array or not
 
-console.log(Array.isArray("Nagendra"));
-//  conclusion :- false;
+// console.log(Array.isArray("Nagendra"));
+// //  conclusion :- false;
 
-// when we want make them array
+// // when we want make them array
 
-console.log(Array.from("Nagendra"));
+// console.log(Array.from("Nagendra"));
 
-/*conclusion :-
-['N', 'a', 'g','e', 'n', 'd','r', 'a']
-*/
+// /*conclusion :-
+// ['N', 'a', 'g','e', 'n', 'd','r', 'a']
+// */
 
-//  when we make 4,5 variable inside a array then :
+// //  when we make 4,5 variable inside a array then :
 
-let cast1 = "Rajput";
-let cast2 = "Shekhawat";
-let cast3 = "Rathore";
-let cast4 = "chouhan";
-console.log(Array.of(cast1,cast2,cast3,cast4));
-//  Conclusion : -
-// [ 'Rajput', 'Shekhawat', 'Rathore', 'chouhan' ]
+// let cast1 = "Rajput";
+// let cast2 = "Shekhawat";
+// let cast3 = "Rathore";
+// let cast4 = "chouhan";
+// console.log(Array.of(cast1,cast2,cast3,cast4));
+// //  Conclusion : -
+// // [ 'Rajput', 'Shekhawat', 'Rathore', 'chouhan' ]
---
+++ object.js
@@ -0,0 +1,23 @@
+//  we create objects like this ;
+let mySym = Symbol("key1");
+let a = Symbol("key2")
+
+let myUser = {
+    name : "kailash Singh",
+    [mySym]: "key1",
+    Dob : "30/01/2001",
+    pob : "Pakistan",
+    occupation : "Aatankwadi",
+    Sangatan : "Alkayda",
+    Aim : "Mumbai Blast",
+    Case : "Murder,Half-murder"
+}
+// console.table(typeof myUser[mySym]);
+// console.log(typeof [mySym]) 
+
+//  => MOST IMPORTANT
+// console.log(typeof([mySym][0]))
+
+// when the interviwer says implement symbol in the object (MOST IMPORTANT)
+// mySym : "key-1" => this way is wrong accordingg this type of string but we want symbol
+
