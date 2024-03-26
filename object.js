fd68a6e3ca0ae5f1f3970553096fa19c116479f5
nagendra_singh <nagendrasinghrawna17@gmail.com>
2024-03-26 01:13:04 -0400

    updated

--- object.js
+++ object.js
@@ -24,4 +24,15 @@ let myUser = {
 // when the interviwer says implement symbol in the object (MOST IMPORTANT)
 // mySym : "key-1" => this way is wrong accordingg this type of string but we want symbol
 
-
+// const tinderuser = new Object()                // singleton object
+/*
+let myUser = {
+    name : "kailash Singh",
+    [mySym]: "key1",
+    Dob : "30/01/2001",
+    pob : "Pakistan",
+    occupation : "Aatankwadi",
+    Sangatan : "Alkayda",
+    Aim : "Mumbai Blast",
+    Case : "Murder,Half-murder"
+} */                                                // object literals
